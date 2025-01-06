e7cad5ad8331f398131b9a5bc04d476b31368ba0
DhananjiMadhushika <dhananjimadushika72@gmail.com>
2025-01-06 11:24:14 +0530

    upload

---
+++ src/main/java/com/hellohr/mpxj/MpxjApplication.java
@@ -0,0 +1,13 @@
+package com.hellohr.mpxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpxjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpxjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellohr/mpxj/MpxjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellohr.mpxj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpxjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
