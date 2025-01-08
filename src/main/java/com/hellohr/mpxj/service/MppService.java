96acdd793fcf2ad98f0fa9fc36d315df26c5b2ff
DimalshaKavindi <dimalshakavindi2020@gmail.com>
2025-01-08 10:28:05 +0530

    commit

--- src/main/java/com/hellohr/mpxj/service/MppService.java
+++ src/main/java/com/hellohr/mpxj/service/MppService.java
@@ -22,67 +22,60 @@ public class MppService {
     @Autowired
     private TaskRepository taskRepository;
 
-    public List<String> parseAndSaveMpp(String filePath) throws Exception {
+    public List<Map<String, Object>> parseAndSaveMpp(String filePath) throws Exception {
         MPPReader reader = new MPPReader();
         ProjectFile projectFile = reader.read(filePath);
 
-        // Create a list to hold the project and task names
-        List<String> projectAndTaskNames = new ArrayList<>();
-
-        // Fetch project properties and save project
+        // Save project details
         Project project = new Project();
         String projectName = projectFile.getProjectProperties().getProjectTitle();
         if (projectName != null) {
             project.setName(projectName);
-            projectAndTaskNames.add("Project: " + projectName);
         }
-
         project.setStartDate(projectFile.getProjectProperties().getStartDate());
         project.setEndDate(projectFile.getProjectProperties().getFinishDate());
         projectRepository.save(project);
 
-        // A map to track tasks by their IDs for parent-child relationships
+        // Map to hold tasks by their IDs for parent-child relationships
         Map<Integer, Task> taskMap = new HashMap<>();
+        List<Map<String, Object>> projectHierarchy = new ArrayList<>();
 
-        // Iterate through tasks and save them
-        for (net.sf.mpxj.Task mpxjTask : projectFile.getTasks()) {
-            if (mpxjTask.getName() != null) {
-                Task taskEntity = new Task();
-                taskEntity.setName(mpxjTask.getName());
-                taskEntity.setStartTime(mpxjTask.getStart());
-                taskEntity.setEndTime(mpxjTask.getFinish());
-
-                // Convert duration to Float
-                if (mpxjTask.getDuration() != null) {
-                    taskEntity.setDuration((float) mpxjTask.getDuration().getDuration());
-                }
-
-                taskEntity.setProject(project);
-
-                // Check if the task has a parent task and set the parent-child relationship
-                if (mpxjTask.getParentTask() != null) {
-                    Task parentTask = taskMap.get(mpxjTask.getParentTask().getID());
-                    if (parentTask != null) {
-                        taskEntity.setParentTask(parentTask); // Set the parent task
-                        parentTask.getSubtasks().add(taskEntity); // Add current task as a subtask of the parent
-                    }
-                }
-
-                // Save the task entity
-                taskRepository.save(taskEntity);
-
-                // Add task name to the list
-                projectAndTaskNames.add("Task: " + taskEntity.getName());
-
-                // Store the task in the map for later reference (for parent-child relation)
-                taskMap.put(mpxjTask.getID(), taskEntity);
-            }
+        // Recursive method to build the task hierarchy
+        for (net.sf.mpxj.Task mpxjTask : projectFile.getChildTasks()) {
+            Map<String, Object> taskHierarchy = processTask(mpxjTask, project, taskMap);
+            projectHierarchy.add(taskHierarchy);
         }
 
-        // Return the list of project and task names
-        return projectAndTaskNames;
+        return projectHierarchy;
     }
 
+    private Map<String, Object> processTask(net.sf.mpxj.Task mpxjTask, Project project, Map<Integer, Task> taskMap) {
+        // Create a task entity and save it
+        Task taskEntity = new Task();
+        taskEntity.setName(mpxjTask.getName());
+        taskEntity.setStartTime(mpxjTask.getStart());
+        taskEntity.setEndTime(mpxjTask.getFinish());
+        if (mpxjTask.getDuration() != null) {
+            taskEntity.setDuration((float) mpxjTask.getDuration().getDuration());
+        }
+        taskEntity.setProject(project);
+        taskRepository.save(taskEntity);
+
+        // Store the task in the map
+        taskMap.put(mpxjTask.getID(), taskEntity);
+
+        // Build the hierarchical structure
+        Map<String, Object> taskHierarchy = new HashMap<>();
+        taskHierarchy.put("taskName", mpxjTask.getName());
+        taskHierarchy.put("subtasks", new ArrayList<Map<String, Object>>());
+
+        for (net.sf.mpxj.Task subTask : mpxjTask.getChildTasks()) {
+            Map<String, Object> subTaskHierarchy = processTask(subTask, project, taskMap);
+            ((List<Map<String, Object>>) taskHierarchy.get("subtasks")).add(subTaskHierarchy);
+        }
+
+        return taskHierarchy;
+    }
 
 }
 
