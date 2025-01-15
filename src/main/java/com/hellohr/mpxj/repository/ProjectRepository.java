de41b64ea7a4fa18c9013f035aa18c2e27c50cb1
DhananjiMadhushika <dhananjimadushika72@gmail.com>
2025-01-15 21:34:05 +0530

    updated

--- src/main/java/com/hellohr/mpxj/repository/ProjectRepository.java
+++ src/main/java/com/hellohr/mpxj/repository/ProjectRepository.java
@@ -2,6 +2,12 @@ package com.hellohr.mpxj.repository;
 
 import com.hellohr.mpxj.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Optional<Project> findByCode(String code);
 }
+
